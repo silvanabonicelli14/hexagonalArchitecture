9d368f065dd53bb8965e850ca5cba0194830ff3d
Silvana Bonicelli <silvana.bonicelli@cgm.com>
2021-05-10 12:30:04 +0200

    First Commit

---
+++ src/test/kotlin/cgm/hexagonal/greetingsKata/GreetingsTests.kt
@@ -0,0 +1,24 @@
+package cgm.hexagonal.greetingsKata
+
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+class GreetingsTests {
+    @Test
+    internal fun name() {
+
+    }
+//    Creo expected con dati cablati in input
+//    PAsso lista a service
+//    passo data a service cablata
+//    test rosso
+//    faccio diventare verde con risultato atteso cablato
+    /*
+    introduco funzione per trovare chi compie anni
+    indroduco fun che fa display messaggio auguri
+
+    */
+
+}
+
+
