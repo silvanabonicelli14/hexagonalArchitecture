b23ddd8560b04f3a912a074e040ce767e3efdc2d
Silvana Bonicelli <silvana.bonicelli@cgm.com>
2021-05-11 08:56:57 +0200

    source string to list of person

--- src/test/kotlin/cgm/hexagonal/greetingsKata/GreetingsTests.kt
+++ src/test/kotlin/cgm/hexagonal/greetingsKata/GreetingsTests.kt
@@ -1,16 +1,25 @@
 package cgm.hexagonal.greetingsKata
 
 import io.kotest.matchers.shouldBe
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVParser
 import org.junit.jupiter.api.Test
+
 
 class GreetingsTests {
     @Test
     internal fun name() {
-        var listOfPerson = """
+        var sourcePerson = """
             last_name, first_name, date_of_birth, email
             Doe, John, 1982/10/08, john.doe@foobar.com
             Ann, Mary, 1975/09/11, mary.ann@foobar.com
         """.trimIndent()
+
+        val csvFormat = CSVFormat.EXCEL.withDelimiter(',').withHeader()
+        val csvParser = CSVParser.parse(sourcePerson, csvFormat)
+        val listOfPerson = csvParser.map { record ->
+            Person(record[1],record[0],record[2],record[3])
+        }
 
         MyService().sendMessagesTo(listOfPerson) shouldBe """
             Subject: Happy birthday!
@@ -20,15 +29,21 @@ class GreetingsTests {
     }
 }
 
+class Person(
+    val firstName: String,
+    val lastName: String,
+    val dateOfBirth: String,
+    val email: String
+)
+
 class MyService {
-    fun sendMessagesTo(listOfPerson: String): String {
+    fun sendMessagesTo(listOfPerson: List<Person>): String {
         return """
             Subject: Happy birthday!
 
             Happy birthday, dear John!
         """.trimIndent()
     }
-
 }
 
 
