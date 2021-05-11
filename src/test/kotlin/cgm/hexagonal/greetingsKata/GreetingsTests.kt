c5930fcd450c47d1f60acc23163900a2586291c1
Silvana Bonicelli <silvana.bonicelli@cgm.com>
2021-05-11 08:53:38 +0200

    Primo test fake

--- src/test/kotlin/cgm/hexagonal/greetingsKata/GreetingsTests.kt
+++ src/test/kotlin/cgm/hexagonal/greetingsKata/GreetingsTests.kt
@@ -1,23 +1,33 @@
 package cgm.hexagonal.greetingsKata
 
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
-import java.time.LocalDate
 
 class GreetingsTests {
     @Test
     internal fun name() {
+        var listOfPerson = """
+            last_name, first_name, date_of_birth, email
+            Doe, John, 1982/10/08, john.doe@foobar.com
+            Ann, Mary, 1975/09/11, mary.ann@foobar.com
+        """.trimIndent()
 
+        MyService().sendMessagesTo(listOfPerson) shouldBe """
+            Subject: Happy birthday!
+
+            Happy birthday, dear John!
+        """.trimIndent()
     }
-//    Creo expected con dati cablati in input
-//    PAsso lista a service
-//    passo data a service cablata
-//    test rosso
-//    faccio diventare verde con risultato atteso cablato
-    /*
-    introduco funzione per trovare chi compie anni
-    indroduco fun che fa display messaggio auguri
+}
 
-    */
+class MyService {
+    fun sendMessagesTo(listOfPerson: String): String {
+        return """
+            Subject: Happy birthday!
+
+            Happy birthday, dear John!
+        """.trimIndent()
+    }
 
 }
 
