b70d20f65783f3fbe72cbf2d081c154719d32f60
Silvana Bonicelli <silvana.bonicelli@cgm.com>
2021-05-12 10:09:03 +0200

    Creazione input salesarticles

--- src/test/kotlin/cgm/hexagonal/salestaxesKata/SalesTaxesTests.kt
+++ src/test/kotlin/cgm/hexagonal/salestaxesKata/SalesTaxesTests.kt
@@ -1,12 +1,14 @@
 package cgm.hexagonal.salestaxesKata
 
 import io.kotest.matchers.shouldBe
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVParser
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.math.roundToLong
 
 class SalesTaxesTests {
-    private val art1 = SaleArticle(Article("ART1",1.0,Category.Food,Country("ITA")),1.0)
-    private val art2 = SaleArticle(Article("ART2",1.0,Category.Other,Country("SPA")),1.0)
     private val expectedArt1 =  SaleArticle(Article("ART1",1.0,Category.Food,Country("ITA")),1.0)
     private val expectedArt2 =  SaleArticle(Article("ART2",1.0,Category.Other,Country("SPA")),1.0)
     init {
@@ -17,18 +19,45 @@ class SalesTaxesTests {
     @Test
     internal fun `create receipt`() {
 
-        val sale = Sale(Country("ITA"),listOf(art1, art2))
-        val receipt = ReceiptService().receipt(sale)
+        val listOfArticles = """
+            cod, price, category, country, quantity
+            ART1, 1.0, Food, ITA, 1
+            ART2, 1.0, Other, SPA, 1
+        """.trimIndent()
+        val sale = getSale(listOfArticles)
+
+        val receipt = ReceiptService(sale).receipt()
 
         receipt.totalPrice shouldBe 2.0
         receipt.totalTax shouldBe 2.0
         receipt.saleArticles.size shouldBe 2
         receipt.saleArticles[0].tax.roundedDouble shouldBe 0.0
         receipt.saleArticles[1].tax.roundedDouble shouldBe 0.15
+        receipt.saleArticles[0].taxedPrice.roundedDouble shouldBe 1.0
+        receipt.saleArticles[1].taxedPrice.roundedDouble shouldBe 1.15
 
         receipt shouldBe   Receipt(2.0,2.0,listOf(expectedArt1,expectedArt2))
     }
+
+    private fun getSale(listOfArticles: String): Sale {
+        val csvFormat = CSVFormat.EXCEL.withDelimiter(',').withHeader()
+        val csvParser = CSVParser.parse(listOfArticles, csvFormat)
+        val listOfSaleArticles: List<SaleArticle> = csvParser.map { record ->
+            SaleArticle(
+                Article(
+                    record[0].trimIndent(),
+                    record[1].trimIndent().toDouble(),
+                    Category.valueOf(record[2].trimIndent()),
+                    Country(record[3].trimIndent())
+                ),
+                record[4].trimIndent().toDouble()
+            )
+        }
+        return Sale(Country("ITA"), listOfSaleArticles)
+    }
 }
+
+
 
 enum class Category {
     Medical,
@@ -43,18 +72,6 @@ val Double.roundedDouble: Double
     }
 
 class Sale(val country: Country,val salesList: List<SaleArticle>) {
-
-    fun getTaxes() {
-        val listOfExemptions  = mutableListOf(Category.Book, Category.Food, Category.Medical)
-        this.salesList.forEach { saleArticle ->
-            if (saleArticle.article.country != this.country) saleArticle.tax += 0.05
-            if (saleArticle.article.category !in (listOfExemptions)) saleArticle.tax += 0.1
-        }
-    }
-
-    fun calculatePrice() {
-
-    }
 }
 
 data class Country(val country: String)
@@ -71,18 +88,18 @@ data class Article(
     val country: Country
 )
 
-class ReceiptService {
-    fun receipt(sale: Sale): Receipt {
+class ReceiptService(private val sale: Sale) {
+    fun receipt(): Receipt {
 
       sale.salesList.forEach {it.tax += getTaxes(it.article,sale.country) }
-      sale.salesList.forEach {calculatePrice(it, it.tax) }
+      sale.salesList.forEach {it.taxedPrice = calculatePrice(it, it.tax) }
 
       return Receipt(2.0, 2.0, sale.salesList)
     }
 
 
-    private fun calculatePrice(article: SaleArticle, tax: Double): Double {
-        return (article.taxedPrice * article.quantity) * (1 + tax)
+    private fun calculatePrice(saleArticle: SaleArticle, tax: Double): Double {
+        return (saleArticle.article.price * saleArticle.quantity) * (1 + tax)
     }
 
     private fun getTaxes(article: Article, countryOfSale: Country): Double {
